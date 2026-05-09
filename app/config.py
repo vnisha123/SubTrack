9ccbaf04061f89f60fcc0aee47592553ab4e926c
Nisha <nishavahutre001@gmail.com>
2026-05-09 21:46:02 +0530

    updated pydantic settings by adding algorithm and access tokens

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     Database_url :str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     model_config = SettingsConfigDict(env_file=".env")
 
